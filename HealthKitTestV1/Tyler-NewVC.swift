c9b8782d7f5972d8ca71561a72be7163927835d8
Tyler Boston <tboston6789@gmail.com>
2021-05-01 19:16:19 -0500

    Created Tyler-VC, added one line of code for print func

---
+++ HealthKitTestV1/Tyler-NewVC.swift
@@ -0,0 +1,26 @@
+//
+//  Tyler-NewVC.swift
+//  HealthKitTestV1
+//
+//  Created by Tyler Boston on 5/1/21.
+//
+
+import UIKit
+
+class Tyler_NewVC: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    
+    func tylerPrintsThis () {
+        
+        print("Hello World, This is my new code")
+    }
+    
+}
